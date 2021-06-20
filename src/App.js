6a6bf079f9286d6edfef77a2da457bfb1e05ccae
Daniel Mikes <daniel.mikes@hva.nl>
2021-06-20 18:24:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Contacts from './components/Contacts'
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        }
+      }).then(data => setContacts(data))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contacts contacts={contacts} />
     </div>
   );
 }
